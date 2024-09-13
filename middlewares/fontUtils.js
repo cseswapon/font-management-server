864c1247aaa2aef5447d04b6ef6b364263fe0f2a
cseswapon <cseswaponsaha@gmail.com>
2024-09-13 23:30:19 +0600

    [MOD] - modify fontutils function

--- middlewares/fontUtils.js
+++ middlewares/fontUtils.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/");
+     cb(null, "/tmp/");
   },
     filename: (req, file, cb) => {
     //   console.log(file?.originalname.split('.')[0]);

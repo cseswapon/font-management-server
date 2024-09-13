501d1e5cd0b232a5dd62f5489535af8698547598
cseswapon <cseswaponsaha@gmail.com>
2024-09-14 00:03:51 +0600

    [MOD] - modify controler file derection

--- controller/fontController.js
+++ controller/fontController.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const fs = require('fs');
-const upload = require("../utils/fontUtils");
+const upload = require("../middlewares/fontUtils");
 
 exports.uploadFont = upload.single("fontFile");
 
